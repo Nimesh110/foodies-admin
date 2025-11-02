5baf4b1f468bc067da2b2dce9c9fb016df70692e
Vidura Nimesh <emoneyvn.info@gmail.com>
2025-11-02 23:33:44 +0530

    Connect admin panel to Railway backend using assets.js

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://foodies-backend-production-2bee.up.railway.app"
 
   return (
     <div>
